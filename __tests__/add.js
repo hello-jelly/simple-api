1ab2275fdbf55f12061059528bdc8516e048d6a9
Angela Vawser <amvawser@gmail.com>
2019-09-25 16:40:15 -0700

    working on testing

---
+++ __tests__/add.js
@@ -0,0 +1,7 @@
+const subtract = (one, two) => {
+  return one - two;
+};
+
+module.exports = {
+  subtract
+};
---
+++ __tests__/add.test.js
@@ -0,0 +1,9 @@
+const test = require('./add');
+
+describe('function subtracts two numbers', () => {
+  it('subtracts', () => {
+    const one = 10;
+    const two = 4;
+    expect(test.subtract(one, two)).toBe(6);
+  });
+});
